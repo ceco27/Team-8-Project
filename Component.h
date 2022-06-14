64fdbe6859c9bb4defa2af6e2be5e6ede72b2ce1
ceco27 <cvetanivanov2002@gmail.com>
2022-06-15 00:11:55 +0300

    Add project files.

---
+++ Component.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class Component {
+protected:
+	enum TypeOfComponent
+	{
+		MOTHERBOARD,
+		CPU,
+		GPU
+	} type;
+	char* manufacturer;
+	char* model;
+	double price;
+public:
+
+	Component();
+	Component(const char*, const char*, const double);
+	Component(const Component&);
+	Component& operator=(const Component&);
+	virtual ~Component();
+
+	double getPrice() const;
+	void setPrice(const double);
+
+	virtual void showComponentInfo() const = 0;
+	virtual TypeOfComponent getType() const;
+
+	virtual Component* clone() const = 0;
+
+private:
+	void copy(const Component&);
+	void free();
+};
